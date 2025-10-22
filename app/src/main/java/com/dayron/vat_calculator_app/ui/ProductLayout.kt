c8b1a8c1e0d574b7f630a4fc010a427d100a2cde
Dayron160 <dayronnotario4@gmail.com>
2025-10-23 00:26:46 +0200

    PRACTICE 2.3 SUBMISSION COMMIT

--- app/src/main/java/com/dayron/vat_calculator_app/ui/ProductLayout.kt
+++ app/src/main/java/com/dayron/vat_calculator_app/ui/ProductLayout.kt
@@ -1,17 +1,24 @@
 package com.dayron.vat_calculator_app.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +28,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -38,10 +49,17 @@ fun ProductLayout() {
     val percentage = (percentageInput.toDoubleOrNull() ?: 0.0)
     val vat = calculateVat(amount, percentage)
 
+    Scaffold (
+        topBar = {
+            VatCalculatorTopAppBar()
+        }
+    ){ it ->
     Column(
         modifier = Modifier
             .statusBarsPadding()
-            .padding(horizontal = 40.dp)
+            .fillMaxSize()
+            .padding(it)
+            .padding(horizontal = dimensionResource(id = R.dimen.padding_large))
             .verticalScroll(rememberScrollState())
             .safeDrawingPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -49,15 +67,17 @@ fun ProductLayout() {
     ) {
         Text(
             text = stringResource(R.string.product),
+            style = MaterialTheme.typography.displayLarge,
             modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
+                .padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(id = R.dimen.padding_large))
                 .align(alignment = Alignment.Start)
         )
         EditField(
             value = productInput,
             onValueChanged = { productInput = it },
             modifier = Modifier
-                .padding(bottom = 8.dp)
+                .padding(bottom = dimensionResource(id = R.dimen.padding_small))
+                .clip(MaterialTheme.shapes.small)
                 .fillMaxWidth(),
             label = R.string.product_name,
             leadingIcon = R.drawable.outline_storefront_24,
@@ -69,7 +89,10 @@ fun ProductLayout() {
         EditField(
             value = amountInput,
             onValueChanged = { amountInput = it },
-            modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(bottom = dimensionResource(id = R.dimen.padding_small))
+                .clip(MaterialTheme.shapes.small)
+                .fillMaxWidth(),
             label = R.string.price,
             leadingIcon = R.drawable.money,
             keyboardOptions = KeyboardOptions(
@@ -80,7 +103,10 @@ fun ProductLayout() {
         EditField(
             value = percentageInput,
             onValueChanged = { percentageInput = it },
-            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(bottom = dimensionResource(id = R.dimen.padding_large))
+                .clip(MaterialTheme.shapes.small)
+                .fillMaxWidth(),
             label = R.string.vat,
             leadingIcon = R.drawable.percent,
             keyboardOptions = KeyboardOptions(
@@ -90,12 +116,39 @@ fun ProductLayout() {
         )
         Text(
             text = stringResource(R.string.total_price, vat),
-            style = MaterialTheme.typography.displaySmall
+            style = MaterialTheme.typography.displayMedium
         )
         Spacer(modifier = Modifier.height(150.dp))
     }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VatCalculatorTopAppBar(
+    modifier: Modifier = Modifier.fillMaxWidth()
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "logo",
+                    modifier = Modifier
+                        .padding(horizontal = dimensionResource(id = R.dimen.image_size))
+                        .size(60.dp),
+                    contentScale = ContentScale.Fit
+                )
+                Text (
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
 private fun calculateVat(amount: Double, vatPercent: Double = 15.0): String {
     val vat = vatPercent / 100 * amount + amount
     val decimalFormat = DecimalFormat("#0.00")
